f863a401b67aa397fa7ca8e7182424b76d96cbaf
timofey <timofey13052005@gmail.com>
2025-09-10 21:13:05 +0300

    feat: added UserPrincipalNotFoundException. implemented loadByUsername() in UserAuthService

---
+++ impl/src/main/java/com/app/impl/service/UserAuthService.java
@@ -0,0 +1,32 @@
+package com.app.impl.service;
+
+import com.app.impl.model.UserPrincipal;
+import com.app.impl.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserAuthService implements UserDetailsService {
+    private UserRepository userRepository;
+
+    @Autowired
+    public void UserAuthService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+        return userRepository.findByLogin(login)
+                .map(user -> new UserPrincipal(
+                        user.getLogin(),
+                        user.getPasswordHash(),
+                        user.getRoles()
+                ))
+                .orElseThrow(() -> new UsernameNotFoundException(String.format("User with login %s was not found", login)));
+    }
+}
