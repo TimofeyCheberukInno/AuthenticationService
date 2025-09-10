230ae5954e8eae5aacd2a1bc3afb14f8d147fdb7
timofey <timofey13052005@gmail.com>
2025-09-10 15:15:15 +0300

    feat: implemented Object methods in User and UserPrincipal. changed UserCredential to record class. added findByLogin() in UserRepository

--- impl/src/main/java/com/app/impl/repository/UserRepository.java
+++ impl/src/main/java/com/app/impl/repository/UserRepository.java
@@ -1,8 +1,11 @@
 package com.app.impl.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.app.impl.entity.User;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserAuthRepository extends JpaRepository<User, Long> {
+    Optional<User> findByLogin(String login);
 }
