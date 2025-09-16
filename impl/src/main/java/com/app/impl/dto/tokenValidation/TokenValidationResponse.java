0a77e1bf4670943d5599141676950896c184c6fd
timofey <timofey13052005@gmail.com>
2025-09-16 16:43:09 +0300

    feat: implemented UserAuthController

--- impl/src/main/java/com/app/impl/dto/tokenValidation/TokenValidationResponse.java
+++ impl/src/main/java/com/app/impl/dto/tokenValidation/TokenValidationResponse.java
@@ -1,9 +1,6 @@
 package com.app.impl.dto.tokenValidation;
 
-import jakarta.validation.constraints.NotBlank;
-
 public record TokenValidationResponse(
     boolean valid,
-    @NotBlank
     String login
 ) { }
